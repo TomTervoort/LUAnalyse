a85729117bd9679c5c38e380791d90b575b4ce3d
Tom Tervoort <tomtervoort@gmail.com>
2013-05-27 14:02:59 +0200

    Improved documentation and such of framework module.

--- src/LUAnalyse/Framework/Framework.hs
+++ src/LUAnalyse/Framework/Framework.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE Haskell2010, FunctionalDependencies, ScopedTypeVariables #-}
 
 
-module LUAnalyse.Framework.Framework where
+module LUAnalyse.Framework.Framework (
+                                        Analysis (..),
+                                        AnalysisType,
+                                        AnalysisDirection,
+                                        performAnalysis
+                                     ) where
 
 import LUAnalyse.Framework.Lattice
 import LUAnalyse.ControlFlow.Flow 
@@ -13,12 +18,25 @@ import qualified Data.Map as M
 import Data.Set (Set)
 import qualified Data.Set as S
 
+-- | Denotes the direction of analysis: either forward or backwards.
 data AnalysisDirection = ForwardAnalysis
                        | BackwardAnalysis
 
+-- | Denotes the type of analysis. A 'must' analysis uses, by default, a lattice top as least 
+--   informative value and uses the lattice meet to restrict is as the analysis progresses.
+--   A 'may' analysis, on the other hand, uses the lattice bottom as least informative value by 
+--   default and extends it by joining.
+-- 
+--   You could say that, in a must analysis information is restricted/specialized while, in a may
+--   analysis, information is extended/generalised.
 data AnalysisType = MustAnalysis
                   | MayAnalysis
 
+-- | A type class representing an analysis of Lua programs within the monothonic framework. Here 
+--   @a@ is a datatype representing the analysis. It may be a simple marker unit type but might
+--   also contain parameters or settings for the analysis. @l@ is a lattice in which information
+--   is stored that is gathered by the analysis (e.g. the available expressions or currently known
+--   types of variables).
 class Lattice l => Analysis a l | a -> l where
  -- | A monotonic transfer function that updates the lattice based on the effects of a Lua 
  --   instruction.
@@ -107,7 +125,9 @@ graphRep :: [(InstructionLabel, InstructionLabel)] -> Map InstructionLabel [Inst
 graphRep = foldr (\(n,e) -> M.alter (addEdge e) n) M.empty
  where addEdge e es = Just $ e : fromMaybe [] es
 
-
+-- | Performs the actual analysis using maximal fixpoint iteration. The result is a mapping of
+--   labels to the eventual state of the lattice before and after exiting the label (in the 
+--   direction of the analysis).
 performAnalysis :: forall a l. Analysis a l => Program -> a -> Map InstructionLabel (l, l)
 performAnalysis p a = addExits $ mfp edges' initialState
  where (atype, adir) = analysisKind a
@@ -120,9 +140,11 @@ performAnalysis p a = addExits $ mfp edges' initialState
        initialVal l | l `S.member` extremals = extremal a
                     | otherwise              = least a
 
+       -- The intial state of the information about each label.
        initialState :: Map InstructionLabel l
        initialState = M.mapWithKey (\l _ -> initialVal l) instructions
 
+       -- MFP iteration, provides the entry value of each label.
        mfp :: [(InstructionLabel, InstructionLabel)] 
                 -> Map InstructionLabel l 
                 -> Map InstructionLabel l
@@ -133,5 +155,7 @@ performAnalysis p a = addExits $ mfp edges' initialState
         where transferred = trans (instructions M.! l) $ m M.! l
               ml' = m M.! l'
 
+       -- Performs the transfer function on each label once more to obtain the exit value along 
+       -- with the entry value.
        addExits :: Map InstructionLabel l -> Map InstructionLabel (l, l)
        addExits = M.mapWithKey $ \i l -> (l, trans (instructions M.! i) l)
